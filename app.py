4f44d138e2f7618a602da9da1d70bf9e9e1a2f5a
Emma Marriott <mekmarriott@gmail.com>
2015-05-01 12:17:47 -0700

    Initial commit

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 """
+References:
+
 Flask Documentation:     http://flask.pocoo.org/docs/
 Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 
-This file creates your application.
 """
 
 import os
 from flask import Flask, render_template, request, redirect, url_for
 
+#=======================================================================
+#       Application Configuration
+#=======================================================================
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'EHR_visualization_2015') #<= not-so-secret key for now
+#=======================================================================
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
-
-
-###
-# Routing for your application.
-###
-
+#=======================================================================
+#       General routing for application
+#=======================================================================
 @app.route('/')
 def home():
     """Render website's home page."""
@@ -28,19 +30,26 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html')
+#=======================================================================
+
+#=======================================================================
+#       Routing for all AJAX calls
+#=======================================================================
+@app.route('/_notes/')
+def notes():
+    """Return all patient note information."""
+    return None
+
+@app.route('/_medications/')
+def medications():
+    """Return all medication information."""
+    return None
+#=======================================================================
 
 
-###
-# The functions below should be applicable to all Flask apps.
-###
-
-@app.route('/<file_name>.txt')
-def send_text_file(file_name):
-    """Send your static text file."""
-    file_dot_text = file_name + '.txt'
-    return app.send_static_file(file_dot_text)
-
-
+########################################################################
+#       Maintenance functions for Flask apps.
+#=======================================================================
 @app.after_request
 def add_header(response):
     """
@@ -51,12 +60,16 @@ def add_header(response):
     response.headers['Cache-Control'] = 'public, max-age=600'
     return response
 
-
 @app.errorhandler(404)
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+#=======================================================================
 
 
+#=======================================================================
+#       RUN APPLICATION!
+#=======================================================================
 if __name__ == '__main__':
     app.run(debug=True)
+#=======================================================================
