8aed4353c386d2ece553b89865e352106fe78810
dewyman <dwyman@stanford.edu>
2015-05-07 20:57:23 -0700

    Started implementing python classes

---
+++ objects.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+class Prescription():
+    '''This class represents...'''
+    def __init__(self):
+        pass
+
+class ClinicalNote():
+    '''This class represents...'''
+    def__init__(self):
+        pass
