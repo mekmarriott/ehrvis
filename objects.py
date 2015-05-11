09c1c66110c341f9664dd0c49cdf602d38655120
dewyman <dwyman@stanford.edu>
2015-05-10 21:39:09 -0700

    Minor changes to objects.py

--- objects.py
+++ objects.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
+# Need to parse query output to store information of interest
 
-class Prescription():
-    '''This class represents...'''
+class MedicationEvent():
+    '''This class ...Type of information represented here is a point, not a block.'''
+
+    def __init__(self, date, medName, practitioner):
+        pass
+
+class MedicationTrack():
+    '''This class represents a specific medication that a patient is taking over time. This encompasses MedicationEvents such as changing the dose or 
+       changing to another brand/generic version of the same compound. I need to think about how to most effectively store MedicationEvents associated with this. '''
+
     def __init__(self):
         pass
 
