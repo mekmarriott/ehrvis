7b12bf035697180993e0243fe7bb9fee9433e0c9
dewyman <dwyman@stanford.edu>
2015-05-15 00:44:21 -0700

    Finally figured out how to parse the JSON query results

--- medications.py
+++ medications.py
@@ -9,8 +9,14 @@ class MedicationEvent():
     '''This class ...Type of information represented here is a point, not a block.'''
 
     def __init__(self, data):
-        self.name = request.args.get('data[medication][display]',"",type=string) #This may not be correct yet
-
+        # This gives the display name of the medication: print data["content"]["medication"]["display"]
+        # Date written: print data["content"]["dateWritten"]
+        # Status: print data["content"]["status"]
+        # Prescriber (right now it is a URL. need to deal with thati): print data["content"]["prescriber"]["reference"]
+        # Dose: print data["content"]["prescriber"]["reference"] 
+        # Duration:
+        # Reason: 
+        # Class: 
 
 #class MedicationTrack():
 #    '''This class represents a specific medication that a patient is taking over time. This encompasses MedicationEvents such as changing the dose or 
@@ -19,23 +25,23 @@ class MedicationEvent():
 #    def __init__(self):
 #        pass
 
-#class ClinicalNote():
-#    '''This class represents...'''
-#    def__init__(self):
-#        pass
-
 
 with open('static/FHIR_Sandbox/medications.json') as data_file:    
     data = json.load(data_file)
     #pprint(data)
+    #need to split the query results into individual entries
+    entryList = data["entry"]
+    for entry in entryList:
+        drug = MedicationEvent(entry)
+        
+        print "-------------------------------------------------------------------------------------------"
+        #x=x.encode("ascii")
+
     #x = request.args.get('data[medication][display]',"",type=string) 
     #x = data["display"]
     #
-    x = data["entry"][0]["title"]
-    #y = data.get("title")i
-    print data.values()
-    for k, v in data.items():
-        print k, v, "\n"
+    #x = data["entry"][0]["title"]
         
     #drug = MedicationEvent(data)
     #print drug.name();
+    
