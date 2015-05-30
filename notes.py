49073ca4ce3d41a0ee0386839f4a8fd95e112cae
Emma Marriott <mekmarriott@gmail.com>
2015-05-29 23:25:01 -0700

    import all notes (instead of one function) and removed a bunch of functions in notes to find conflict

--- notes.py
+++ notes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # Need to parse query output to store information of interest
 
-# from flask import Flask, request, json
+from flask import Flask, request, json
 from dateutil import parser
 from pprint import pprint
 from datetime import datetime, timedelta
@@ -75,223 +75,223 @@ class NoteHistory(object):
 
 
 
-    def compile_hospitalizations(self):
-        try:
-            if not self.notes:
-                print "cannot compile hospitalizations from empty note list \n"
-                return
+#     def compile_hospitalizations(self):
+#         try:
+#             if not self.notes:
+#                 print "cannot compile hospitalizations from empty note list \n"
+#                 return
 
-            ks = self.notes.keys()
-            ks.sort()
+#             ks = self.notes.keys()
+#             ks.sort()
 
 
-            # current key index
-            idx = -1
+#             # current key index
+#             idx = -1
             
-            # number of keys
-            max_ = len(ks)
+#             # number of keys
+#             max_ = len(ks)
 
-            hosp = []
+#             hosp = []
 
-            while idx < max_-1:
-                idx+=1
+#             while idx < max_-1:
+#                 idx+=1
 
-                # look for next inpatient note. this defines the start date of the hospitalization
-                if self.notes[ks[idx]].inpatient or "H&P" in self.notes[ks[idx]]._type:
-                    hosp.append(date2utc(self.notes[ks[idx]].time))
-                    print hosp
+#                 # look for next inpatient note. this defines the start date of the hospitalization
+#                 if self.notes[ks[idx]].inpatient or "H&P" in self.notes[ks[idx]]._type:
+#                     hosp.append(date2utc(self.notes[ks[idx]].time))
+#                     print hosp
 
-                    while idx < max_:
-                        # look for next outpatient note. this defines the end date of the hospitalization
-                        idx += 1 
-                        if idx == max_:
-                            # if reach end of notes without reverting to outpatient, put today's date as end date
-                            hosp.append(date2utc(self.maxDate))
-                            print hosp
+#                     while idx < max_:
+#                         # look for next outpatient note. this defines the end date of the hospitalization
+#                         idx += 1 
+#                         if idx == max_:
+#                             # if reach end of notes without reverting to outpatient, put today's date as end date
+#                             hosp.append(date2utc(self.maxDate))
+#                             print hosp
 
-                            # append current hospitalization to hospitalization list
-                            self.hospitalizations.append(hosp)
+#                             # append current hospitalization to hospitalization list
+#                             self.hospitalizations.append(hosp)
 
-                            hosp = []
+#                             hosp = []
 
-                            # exit while loop 
-                            break  
+#                             # exit while loop 
+#                             break  
 
-                        if "Discharge" in self.notes[ks[idx]]._type:
-                            # append time from CURRENT note to "hosp" as end date
-                            hosp.append(date2utc(self.notes[ks[idx]].time))
+#                         if "Discharge" in self.notes[ks[idx]]._type:
+#                             # append time from CURRENT note to "hosp" as end date
+#                             hosp.append(date2utc(self.notes[ks[idx]].time))
 
-                            print hosp
+#                             print hosp
 
-                            # append current hospitalization to hospitalization list
-                            self.hospitalizations.append(hosp)
+#                             # append current hospitalization to hospitalization list
+#                             self.hospitalizations.append(hosp)
 
-                            hosp = []
+#                             hosp = []
 
-                            # exit while loop 
-                            break
+#                             # exit while loop 
+#                             break
 
-                        if not self.notes[ks[idx]].inpatient:
+#                         if not self.notes[ks[idx]].inpatient:
 
-                            # append time from PREVIOUS note to "hosp" as end date
-                            hosp.append(date2utc(self.notes[ks[idx-1]].time))
+#                             # append time from PREVIOUS note to "hosp" as end date
+#                             hosp.append(date2utc(self.notes[ks[idx-1]].time))
 
-                            print hosp
+#                             print hosp
 
-                            # append current hospitalization to hospitalization list
-                            self.hospitalizations.append(hosp)
+#                             # append current hospitalization to hospitalization list
+#                             self.hospitalizations.append(hosp)
 
-                            hosp = []
+#                             hosp = []
 
-                            # exit while loop 
-                            break
+#                             # exit while loop 
+#                             break
 
 
  
-            return
-        except:
-            print "error while attempting to compile hospitalizations from note list \n"
-            return                                             
+#             return
+#         except:
+#             print "error while attempting to compile hospitalizations from note list \n"
+#             return                                             
 
 
 
-    def add_notes(self, note_array):
-        try:
-            for note in note_array:
-                if type(note) is NoteEntry:
+#     def add_notes(self, note_array):
+#         try:
+#             for note in note_array:
+#                 if type(note) is NoteEntry:
 
 
 
-                    # maintain a dictionary of notes: key = timestamp, value = note
-                    # -------------------------------------------------------------
-                    self.notes[str(note.time)]=note
+#                     # maintain a dictionary of notes: key = timestamp, value = note
+#                     # -------------------------------------------------------------
+#                     self.notes[str(note.time)]=note
 
 
-                    # maintain a earliest and latest dates in note history
-                    # ----------------------------------------------------
+#                     # maintain a earliest and latest dates in note history
+#                     # ----------------------------------------------------
 
-                    # 1. get note timestamp
-                    t = note.time
+#                     # 1. get note timestamp
+#                     t = note.time
 
-                    # 2. compare to latest and earliest dates, modify either if superseded
-                    if t != "n/a":
-                        if t < self.minDate:
-                            self.minDate = t
-                        if t > self.maxDate:
-                            self.maxDate = t                    
+#                     # 2. compare to latest and earliest dates, modify either if superseded
+#                     if t != "n/a":
+#                         if t < self.minDate:
+#                             self.minDate = t
+#                         if t > self.maxDate:
+#                             self.maxDate = t                    
 
-                    # maintain a dictionary of events on each day: key = active calendar day, value = count
-                    # -------------------------------------------------------------------------------------
+#                     # maintain a dictionary of events on each day: key = active calendar day, value = count
+#                     # -------------------------------------------------------------------------------------
 
 
-                    # 1. make dict entry for date if doesn't exist yet, key = date, initial value = 0
-                    if str(t.date()) not in self.notesByDate:
-                        self.notesByDate[str(t.date())]=0
+#                     # 1. make dict entry for date if doesn't exist yet, key = date, initial value = 0
+#                     if str(t.date()) not in self.notesByDate:
+#                         self.notesByDate[str(t.date())]=0
 
-                    # 2. increment count 
-                    self.notesByDate[str(t.date())]+=1
+#                     # 2. increment count 
+#                     self.notesByDate[str(t.date())]+=1
 
-                    # 3. associate note count (by day) information with note
-                    # this is the ith (i=current array length) note of the day; we will plot note at height proportional to i
-                    note.heightcount = self.notesByDate[str(t.date())]
+#                     # 3. associate note count (by day) information with note
+#                     # this is the ith (i=current array length) note of the day; we will plot note at height proportional to i
+#                     note.heightcount = self.notesByDate[str(t.date())]
 
      
 
 
-                    # add to [time,height] pair to corresponding dataseries (by service)
-                    # ------------------------------------------------------------------
+#                     # add to [time,height] pair to corresponding dataseries (by service)
+#                     # ------------------------------------------------------------------
 
-                    # 1. get service
-                    s = self.standard_service(note.service)
+#                     # 1. get service
+#                     s = self.standard_service(note.service)
 
-                    # 2. make dict entry for service if doesn't exist, key = service, value = plot data and color
-                    if s not in self.series:
-                        self.series[s]={'label':s,'color':self.serv2color[s],'data':[]}
+#                     # 2. make dict entry for service if doesn't exist, key = service, value = plot data and color
+#                     if s not in self.series:
+#                         self.series[s]={'label':s,'color':self.serv2color[s],'data':[]}
 
-                    # 3. append [time,height] pair to relevant data series
-                    self.series[s]['data'].append([date2utc(t),note.heightcount])
+#                     # 3. append [time,height] pair to relevant data series
+#                     self.series[s]['data'].append([date2utc(t),note.heightcount])
 
-                    # 4. assign note ID by its position within the note series (use 0-based indexing)
-                    _id = len(self.series[s]['data'])-1
+#                     # 4. assign note ID by its position within the note series (use 0-based indexing)
+#                     _id = len(self.series[s]['data'])-1
 
-                    note._id = _id   
+#                     note._id = _id   
 
 
-                    # maintain dictionaries of notes and note previews by service
-                    # ---------------------------------------
+#                     # maintain dictionaries of notes and note previews by service
+#                     # ---------------------------------------
 
-                    # 1. make dict entry for service if doesn't exist, key = service, value = note dict by ID
-                    if s not in self.notesByService:
-                        self.notesByService[s]={}
-                        self.previewsByService[s]={}
+#                     # 1. make dict entry for service if doesn't exist, key = service, value = note dict by ID
+#                     if s not in self.notesByService:
+#                         self.notesByService[s]={}
+#                         self.previewsByService[s]={}
 
-                    # 2. add current note to corresponding dictionary position
-                    self.notesByService[s][_id]=note
+#                     # 2. add current note to corresponding dictionary position
+#                     self.notesByService[s][_id]=note
                     
-                    # 3. add preview of current note to corresponding dictionary position
-                    self.previewsByService[s][_id]=note.to_dict()
+#                     # 3. add preview of current note to corresponding dictionary position
+#                     self.previewsByService[s][_id]=note.to_dict()
 
-            # add viewing buffer to latest and earliest times
-            self.minDate = self.minDate - datetime.timedelta(days=1)
-            self.maxDate = self.minDate + datetime.timedelta(days=1)
+#             # add viewing buffer to latest and earliest times
+#             self.minDate = self.minDate - datetime.timedelta(days=1)
+#             self.maxDate = self.minDate + datetime.timedelta(days=1)
 
-            return 
-        except:
-            print "Malformed data for NoteHistory object population"
-            return 
+#             return 
+#         except:
+#             print "Malformed data for NoteHistory object population"
+#             return 
 
 
 
-def initialize_mimic(entry):
-    try:
-        service = entry["service"]
-        preview = entry["preview"]
-        fulltext = entry["fulltext"]
-        _id = entry["visObject"]["id"]
-        start = entry["visObject"]["start"]
-        start = parser.parse(start)
-        group = entry["visObject"]["group"]
+# def initialize_mimic(entry):
+#     try:
+#         service = entry["service"]
+#         preview = entry["preview"]
+#         fulltext = entry["fulltext"]
+#         _id = entry["visObject"]["id"]
+#         start = entry["visObject"]["start"]
+#         start = parser.parse(start)
+#         group = entry["visObject"]["group"]
         
-        return NoteEntry(service, preview, fulltext, _id, start, group)
-    except:
-        print "Malformed data for NoteEntry object population"
-        return None
+#         return NoteEntry(service, preview, fulltext, _id, start, group)
+#     except:
+#         print "Malformed data for NoteEntry object population"
+#         return None
 
-def initialize_epic(entry):
-    try:
-        service = entry["service"]
-        preview = entry["preview"]
-        fulltext = entry["fulltext"]
-        _id = entry["visObject"]["id"]
-        time = entry["visObject"]["start"]
-        time = parser.parse(time)
-        _type = entry["type"]
-        inpatient = entry["inpatient"]=="true" or entry["inpatient"]=="True" 
-        return NoteEntry(service,preview,fulltext,_id,time,_type,inpatient)
-    except:
-        print "Malformed data for NoteEntry object population"
-        return None    
+# def initialize_epic(entry):
+#     try:
+#         service = entry["service"]
+#         preview = entry["preview"]
+#         fulltext = entry["fulltext"]
+#         _id = entry["visObject"]["id"]
+#         time = entry["visObject"]["start"]
+#         time = parser.parse(time)
+#         _type = entry["type"]
+#         inpatient = entry["inpatient"]=="true" or entry["inpatient"]=="True" 
+#         return NoteEntry(service,preview,fulltext,_id,time,_type,inpatient)
+#     except:
+#         print "Malformed data for NoteEntry object population"
+#         return None    
 
-def load_epic_notes():
-    entryList = json.load(open('static/Note_Sandbox/notes.json'))
-    returnList = []
-    for i,entry in enumerate(entryList):
-        returnList.append(initialize_epic(entry))
-        returnList[-1]._id=i
-    history = NoteHistory();
-    history.add_notes(returnList)
-    history.compile_hospitalizations()
-    return history    
+# def load_epic_notes():
+#     entryList = json.load(open('static/Note_Sandbox/notes.json'))
+#     returnList = []
+#     for i,entry in enumerate(entryList):
+#         returnList.append(initialize_epic(entry))
+#         returnList[-1]._id=i
+#     history = NoteHistory();
+#     history.add_notes(returnList)
+#     history.compile_hospitalizations()
+#     return history    
 
-def load_mimic_notes():
-    entryList = json.load(open('static/Note_Sandbox/mimic_notes.json'))
-    returnList = []
-    for i,entry in enumerate(entryList):
-        returnList.append(initialize_mimic(entry))
-        returnList[-1].visObject["id"]=i
-    history = NoteHistory();
-    history.add_notes(returnList)
-    return history
+# def load_mimic_notes():
+#     entryList = json.load(open('static/Note_Sandbox/mimic_notes.json'))
+#     returnList = []
+#     for i,entry in enumerate(entryList):
+#         returnList.append(initialize_mimic(entry))
+#         returnList[-1].visObject["id"]=i
+#     history = NoteHistory();
+#     history.add_notes(returnList)
+#     return history
 
 
 
