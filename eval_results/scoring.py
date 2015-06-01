0c2e154a9a1dd4204a423b99f47df6361317480b
dewyman <dwyman@stanford.edu>
2015-05-31 17:05:47 -0700

    Added method to do SU scoring on any table and return associated statistics

--- eval_results/scoring.py
+++ eval_results/scoring.py
@@ -1,11 +1,11 @@
 
 import sys
+import numpy as np
 
 def main():
     infile = sys.argv[1]
     standardSUS, ehrvisSUS, adlEhrvis = process(infile)
-    print standardSUS
-
+    calcSUscore(standardSUS)
 
 def process(infile):
     '''Read in results file and create 3 tables based on question type:
@@ -37,6 +37,35 @@ def process(infile):
    
 def calcSUscore(t):
     '''Reads in a table of SUS question answers and calculates the System Usability Score'''
-    
+    # Remove background info since we only want scores
+    t = [ row[3:] for row in t ]
+
+    indivScores = []
+    avgSU = 0
+
+    for i in range(0,len(t)):	#Loop over rows
+        currSU = 0
+        for j in range(0,len(t[0])):	#Loop over cols
+            numQ = j + 1
+            if numQ % 2 == 0:	
+                # If the question number is even, the score contribution is 5 minus the answer
+                ans = int(t[i][j])
+                ans = 5 - ans
+                currSU = currSU + ans
+            else:
+                # If the question number is odd, the score contribution is the answer - 1
+                ans = int(t[i][j])
+                ans = ans - 1 
+                currSU = currSU + ans
+        indivScores.append(2.5*currSU)    
+    print indivScores
+    avgSU = np.mean(indivScores) 
+    medSU = np.median(indivScores)
+    sd = np.std(indivScores)
+    print medSU
+    print avgSU
+    print sd
+    return avgSU, medSU, sd, indivScores
+
 
 main()
